700db36b2c515ae48d3c9335d4b1bf5c1b2de0c3
Graham Fairweather <xotic750@gmail.com>
2019-07-17 15:24:26 +0200

    :bookmark: v2.0.7
    Auto build

--- dist/modulo-x.js
+++ dist/modulo-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T12:57:03.047Z",
+  "date": "2019-07-17T13:24:21.875Z",
   "describe": "",
   "description": "modulo - floored division implementation.",
   "file": "modulo-x.js",
   "hash": "9ef0af63e8959c37986a",
   "license": "MIT",
-  "version": "2.0.6"
+  "version": "2.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
