6a0ef1350ed60077d38c7b499120d25f7961786a
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:11:18 +0200

    :bookmark: v2.0.17
    Add jsdelivr

--- dist/modulo-x.js
+++ dist/modulo-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-23T23:31:16.583Z",
+  "date": "2019-07-24T13:11:13.676Z",
   "describe": "",
   "description": "modulo - floored division implementation.",
   "file": "modulo-x.js",
   "hash": "9ef0af63e8959c37986a",
   "license": "MIT",
-  "version": "2.0.16"
+  "version": "2.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
