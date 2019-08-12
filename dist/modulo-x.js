51b7ebee45472b93529183ed54900d60e008a64c
Graham Fairweather <xotic750@gmail.com>
2019-08-12 23:35:12 +0200

    :bookmark: v2.0.23
    Auto run

--- dist/modulo-x.js
+++ dist/modulo-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-07T21:36:38.451Z",
+  "date": "2019-08-12T21:35:06.812Z",
   "describe": "",
   "description": "modulo - floored division implementation.",
   "file": "modulo-x.js",
